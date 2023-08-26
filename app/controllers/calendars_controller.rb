09a662dbe79367da262262504da826d830496218
reon0921 <reonshankou40@gmail.com>
2023-08-26 22:06:53 +0900

    差分

--- app/controllers/calendars_controller.rb
+++ app/controllers/calendars_controller.rb
@@ -8,11 +8,10 @@ class CalendarsController < ApplicationController
 
   # 予定の保存
   def create
-    //Plan.create(plan_params)
+    Plan.create(plan_params)
     redirect_to action: :index
   end
 
-  
   private
 
   def plan_params
@@ -35,11 +34,10 @@ class CalendarsController < ApplicationController
       plans.each do |plan|
         today_plans.push(plan.plan) if plan.date == @todays_date + x
       end
-      days = { :month => (@todays_date + x).month, :date : (@todays_date+x).day, :plans: today_plans}
+      days = { :month => (@todays_date + x).month, :date => (@todays_date+x).day, :plans => today_plans}
       @week_days.push(days)
     end
     
-
 
   end
 end
