14ae5dc4af6a045294d379e67d36411aa367015d
reon0921 <reonshankou40@gmail.com>
2023-08-31 20:36:26 +0900

    Issue4.5.6

--- app/controllers/calendars_controller.rb
+++ app/controllers/calendars_controller.rb
@@ -8,8 +8,8 @@ class CalendarsController < ApplicationController
 
   # 予定の保存
   def create
-
-  Plan.create(plan_params)
+    binding.pry
+  plan.create(plan_params)
 
     redirect_to action: :index
   end
